f4eadddbac90934832e87393d069ed8bd92063c5
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-04-27 17:12:35 +0700

    update

--- CuaHangMyPham/app/src/main/java/com/example/chmypham/MainActivity.java
+++ CuaHangMyPham/app/src/main/java/com/example/chmypham/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.chmypham;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.chmypham.Activity.DangNhapActivity;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(getApplicationContext(), DangNhapActivity.class));
+        finish();
     }
 }
